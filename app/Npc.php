63469c4d8fdb6191446b3dd7e6e8eb890cf5d26f
Lucas Gouvêa (K2) <lucas.k2@jupiter>
2018-06-16 15:44:13 -0300

    item-database | iniciando modelagem p/ monstros, itens e npcs, populando alguns relacionamentos de npc primeiro. Agora tenho que modelar no banco pra popular as migrations

--- app/Npc.php
+++ app/Npc.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Npc extends Model
 {
-    //
+    protected $table = 'npcs';
+
+    private function script() {
+        return $this->hasOne('App\Script');
+    }
+
+    public function buys() {
+        return $this->hasMany('App\Item', 'id_global');
+    }
+
+    public function sells()
+    {
+        return $this->hasMany('App\Item', 'id_global');
+    }
+
 }
